bea9f059e962b00e27d1602165549bc34158edf7
KingGjd <king.gjd2012@gmail.com>
2012-07-13 16:34:47 +0800

    update secret

---
+++ app/controllers/sync_controller.rb
@@ -0,0 +1,28 @@
+class SyncController < ApplicationController
+
+  def new
+    client = OauthChian::Sina.new
+    authorize_url = client.authorize_url
+    Rails.cache.write(build_oauth_token_key(client.name, client.oauth_toke), client.dump)
+    redirect_to authorize_url
+  end
+
+  def callback
+    client = OauthChian::Sina.load(Rails.cache.read(build_oauth_token_key(params[:type], params[:oauth_token])))
+    client.authorize(:oauth_verifier => params[:oauth_verifier])
+
+    results = client.dump
+
+    if results[:access_token] && results[:access_token_secret]
+      flash[:notice] = "授权成功"
+    else
+      flash[:notice] = "授权失败"
+    end
+    redirect_to root_path
+  end
+
+  private
+  def build_oauth_token_key(name, oauth_token)
+    [name, oauth_token].join("_")
+  end
+end
